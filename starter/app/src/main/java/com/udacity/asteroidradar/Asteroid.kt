1f08f149615ba2e474ac48bb477414eeb055a742
Sabina <sabinaleitoiu91@gmail.com>
2021-02-27 13:38:45 +0000

    first commit

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
@@ -0,0 +1,32 @@
+package com.udacity.asteroidradar
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "daily_asteroid_tracker_table")
+data class Asteroid(
+    @PrimaryKey(autoGenerate = true)
+                val id: Long,
+
+    @ColumnInfo(name = "code_name")
+                val codename: String,
+
+    @ColumnInfo(name = "close_approach_date")
+                val closeApproachDate: String,
+
+    @ColumnInfo(name = "absolute_magnitude")
+                val absoluteMagnitude: Double, val estimatedDiameter: Double,
+
+    @ColumnInfo(name = " relative_velocity")
+                val relativeVelocity: Double,
+
+    @ColumnInfo(name = "distance_from_earth")
+    val distanceFromEarth: Double,
+
+    @ColumnInfo(name = "is_potentially_hazardous")
+    val isPotentiallyHazardous: Boolean)
+    : Parcelable
--- starter/app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabaseDao.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabaseDao.kt
@@ -1,4 +1,24 @@
 package com.udacity.asteroidradar.database
 
-class AsteroidDao {
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.udacity.asteroidradar.Asteroid
+
+@Dao
+interface AsteroidDatabaseDao {
+
+    @Insert
+    fun insert(asteroid: Asteroid)
+
+    @Update
+    fun update(asteroid: Asteroid)
+
+    @Query("SELECT * FROM daily_asteroid_tracker_table WHERE id = :key")
+    fun get(key: Long): Asteroid
+
+    @Query("DELETE FROM daily_asteroid_tracker_table")
+    fun clear()
+
+    @Query("SELECT * FROM daily_asteroid_tracker_table ORDER BY id DESC ")
+    fun getAllAsteroids(): LiveData<List<Asteroid>>
 }
